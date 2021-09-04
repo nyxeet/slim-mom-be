131263019903bd7f26371c5a3740cb43fc62eb81
Ann Maksay <annaorlova.dn@gmail.com>
2021-09-04 15:22:40 +0300

    add logout

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const jwtAuthenticate = require("../middlewares/passport");
 
 const signup = require("../controllers/user/signup");
 const login = require("../controllers/user/login");
+const logout = require("../controllers/user/logout");
 
 const getCalories = require("../controllers/user/getCalories");
 
@@ -12,7 +14,7 @@ const validateGetCalories = require("../validation/user/getCalories");
 
 router.post("/signup", validateNewUser, signup);
 router.post("/login", validateLoginUser, login);
+router.post("/logout", jwtAuthenticate, logout);
 router.get("/getCalories", validateGetCalories, getCalories);
-
 
 module.exports = router;
