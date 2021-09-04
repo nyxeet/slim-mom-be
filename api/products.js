3fb6d6a0763c91156577108c4ed8e018fec76566
Ann Maksay <annaorlova.dn@gmail.com>
2021-09-04 14:38:24 +0300

    controller for serch products by query

--- api/products.js
+++ api/products.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 const getByName = require("../controllers/product/getByName");
 
-router.get("/:productName", getByName);
+router.get("/", getByName);
 
 module.exports = router;
