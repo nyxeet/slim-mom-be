7bb319b42145ef5792bd04f1cee9ff2372d3ed77
Ann Maksay <annaorlova.dn@gmail.com>
2021-09-05 15:29:17 +0300

    add swagger

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
+
 require("./config/passport");
 
 const userRouter = require("./routes/user");
@@ -15,6 +18,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/api/user", userRouter);
 app.use("/api/days", daysRouter);
 app.use("/api/product", productRouter);
